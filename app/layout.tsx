c8f219d1ff45595505312a3e14b9cbb99a9bd0f1
Orezime Abed <orezimeabed2006@gmail.com>
2026-09-22 20:30:22 +0100

    feat(analytics): integrate Google Analytics 4 (GA4) with measurement ID G-P554J4HTVD

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,6 +50,9 @@ export const metadata: Metadata = {
 
 import { ToastProvider } from "@/components/ToastProvider";
 import InstallPrompt from "@/components/InstallPrompt";
+import Script from "next/script";
+
+const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_ID || "G-P554J4HTVD";
 
 export default function RootLayout({
   children,
@@ -145,6 +148,29 @@ export default function RootLayout({
         />
       </head>
       <body>
+        {/* Google Analytics 4 (GA4) */}
+        {GA_MEASUREMENT_ID && (
+          <>
+            <Script
+              strategy="afterInteractive"
+              src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+            />
+            <Script
+              id="google-analytics"
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${GA_MEASUREMENT_ID}', {
+                    page_path: window.location.pathname,
+                  });
+                `,
+              }}
+            />
+          </>
+        )}
         <ToastProvider>
           {children}
           <InstallPrompt />
--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,12 @@ import type { NextConfig } from "next";
 
 const cspHeader = `
   default-src 'self';
-  script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.pusher.com https://apis.google.com https://cdnjs.cloudflare.com;
+  script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.pusher.com https://apis.google.com https://cdnjs.cloudflare.com https://www.googletagmanager.com;
   style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdnjs.cloudflare.com;
   font-src 'self' https://fonts.gstatic.com https://cdnjs.cloudflare.com data:;
   img-src 'self' data: blob: https: http:;
   media-src 'self' data: blob: https:;
-  connect-src 'self' https://*.pusher.com wss://*.pusher.com https://api.resend.com https://*.r2.cloudflarestorage.com https://*.upstash.io;
+  connect-src 'self' https://*.pusher.com wss://*.pusher.com https://api.resend.com https://*.r2.cloudflarestorage.com https://*.upstash.io https://*.google-analytics.com https://*.analytics.google.com https://*.googletagmanager.com;
   frame-ancestors 'none';
   base-uri 'self';
   form-action 'self';
